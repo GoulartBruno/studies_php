b2222e3c2de4cd9372f5cdf349182509a7ef49a9
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-13 16:26:29 +0200

    studies

---
+++ 5_expressoes_e_operadores/2_exercicio_10/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$a = 5;
+$b = 3;
+$c = 8;
+$d = 6;
+
+$soma = $a + $b + $c;
+
+echo $soma;
+
+echo '<br>';
+
+$subtracao = $a - $b - $c - $d;
+
+
+echo $subtracao;
+
+echo '<br>';
+
+$divisao = $a + $b + $c / $d;
+
+
+echo $divisao;
+
+echo '<br>';
+
+$multiplicacao = $a * $b * $c * $d;
+
+
+echo $multiplicacao;
+
+
+echo '<br>';
+
+$geral = $a + $b - $c + $d / $a;
+
+
+echo $geral;
+
+
+echo '<br>';
