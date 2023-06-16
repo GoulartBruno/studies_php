200974da7064c2e65894c51f53299072068e00ed
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-16 17:01:50 +0200

    studies php

---
+++ 5_expressoes_e_operadores/6_exercicio_12/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$a = 10;
+$b = 5;
+$c = 18;
+$d = 7;
+
+$op01 = $a + $b;
+
+echo $op01;
+
+echo "<br><br>";
+
+$op02 = $op01 - $c;
+echo $op02;
+
+echo "<br><br>";
+
+$op03 = $op02 * $a;
+echo $op03;
+
+echo "<br><br>";
+
+$op04 = $op03 / $a;
+echo $op04;
+
+echo "<br><br>";
+
+echo $op01 . " " . $op02 . " " . $op03 . " " . $op04;
