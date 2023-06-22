6bc809660bc4b49a272b18021e6d796d0d63439a
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-22 17:51:05 +0200

    studies php

---
+++ 5_expressoes_e_operadores/13_operadores_de_igualdade/index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+if( 3 == 3){
+echo "Comparação verdadeira <br>";
+
+}
+
+if( 3 == 4){
+  echo "Comparação verdadeira <br>";
+  
+  }
+
+  $a = 12;
+  $b = 12;
+  $c = 100;
+
+
+  if( $a == $b){
+    echo "Comparação 1 é verdadeira <br>";
+    
+    }
+
+    if( $a == $c){
+      echo "Comparação 2 é verdadeira <br>";
+      
+      }
+
+      $nome = "Bruno";
+      $nomdeDoSistema = "Bruno";
+
+      if( $nome == $nomdeDoSistema){
+        echo "Comparação de nomes é verdadeira <br>";
+        
+        }
