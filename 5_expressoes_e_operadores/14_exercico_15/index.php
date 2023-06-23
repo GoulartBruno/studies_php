c0ae43f6c1ef0ba2f89a834540099f45c323404d
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-23 17:58:01 +0200

    studies php

---
+++ 5_expressoes_e_operadores/14_exercico_15/index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+$a = 15;
+$b = 25;
+$c = 15;
+
+if($a == $b){
+  echo "a é igual a b";
+};
+
+echo "<br>";
+
+if($a == $c){
+  echo "a é igual a c";
+}
+
+echo "<br>";
+
+
+if($a !== $b){
+  echo "a é diferente a b";
+}
+
+echo "<br>";
+
+
+if($a !== $c){
+  echo "a é diferente a c";
+}
+
+if($b !== $c){
+  echo "b é diferente a c";
+}
+
+echo "<br>";
+
+
+if($b == $c){
+  echo "b é igual a c";
+}
