5e54d446579a04f4eef66e0e717ab60fd5ae7fa8
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-26 07:14:11 +0200

    studies php

---
+++ 5_expressoes_e_operadores/16_operador_identico_a/index.php
@@ -0,0 +1,31 @@
+<?php
+
+if(5 == "5"){
+
+  echo " 5 é igual a '5' <br>";
+}
+
+if(5 === "5"){
+
+  echo " 5 é igual a '5' <br>";
+}
+
+
+if(5 === 5){
+
+  echo " 5 é igual a 5 <br>";
+}
+
+if(5 === "teste"){
+
+  echo " 5 é igual a teste <br>";
+}
+
+$a = 4;
+$b = 4;
+
+if($a === $b){
+
+  echo " $a é igual a $b <br>";
+
+}
