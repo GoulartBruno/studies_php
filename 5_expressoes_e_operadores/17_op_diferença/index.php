54cf6136a796bdfeda0225b8ac6039a1e795ce9e
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-27 07:20:15 +0200

    studies php

---
+++ 5_expressoes_e_operadores/17_op_diferença/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 3;
+$b = 4;
+$c = 3;
+
+if($a !== $b){
+  echo "$a é diferente de $b <br>";
+}
+
+if($a !== $c){
+  echo "$a é diferente de $c <br>";
+}
+
+if($a !== 5){
+  echo "$a é diferente de 5 <br>";
+}
+
+
+if($a !== 3){
+  echo "$a é diferente de 3 <br>";
+}
+if($a !== "3"){
+  echo "$a é diferente de '3' <br>";
+}
