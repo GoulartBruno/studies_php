614bdd7ca7e3ae9f5c428fd6d69b54de1055558a
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-28 07:25:33 +0200

    studies php

---
+++ 5_expressoes_e_operadores/18_op_nao_identico/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$a = 3;
+$b = 5;
+$c = 3;
+$d = "3";
+
+
+
+if ($a !== $b){
+  echo "$a é diferente de $b <br>";
+
+}
+
+if ($a !== $c){
+  echo "$a é diferente de $c <br>";
+
+}
+
+if ($a !== $d){
+  echo "$a é diferente de '$d' <br>";
+
+}
