7eca3c6efb597a5e64fbf40677329887049eeeca
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-29 07:31:19 +0200

    studies php

---
+++ 5_expressoes_e_operadores/19_exercicio_16/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$a = 5;
+$b = 3;
+
+if($a == $b){
+  echo "$a é igual a $b <br>";
+}
+
+if($a != $b){
+  echo "$a é diferente a $b <br>";
+}
+
+if($a === $b){
+  echo "$a é identido a $b <br>";
+}
+
+if($a !== $b){
+  echo "$a não é identico a $b <br>";
+}
