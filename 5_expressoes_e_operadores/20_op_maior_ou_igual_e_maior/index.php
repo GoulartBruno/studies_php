98b2c1d2385ae61c732454d3f621ef6486a7f7e0
GoulartBruno <brunogoulartdev@gmail.com>
2023-06-30 07:38:41 +0200

    studies php

---
+++ 5_expressoes_e_operadores/20_op_maior_ou_igual_e_maior/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$a = 4;
+$b = 5;
+$c = 6;
+$d = 4;
+
+if($a >= $b){
+
+  echo "$a é maior que $b <br>";
+}
+
+if($b >= $a){
+
+  echo "$b é maior que $a <br>";
+}
+
+if($c >= $a){
+
+  echo "$c é maior que $a <br>";
+}
+
+if($b >= $c){
+
+  echo "$b é maior que $c <br>";
+}
+
+if($a >= $d){
+
+  echo "$a é igual ou maior que $d <br>";
+}
