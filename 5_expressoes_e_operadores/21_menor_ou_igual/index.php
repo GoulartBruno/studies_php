450ae40905200d977dc3954ff31f35158232c643
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-01 07:44:25 +0200

    studies php

---
+++ 5_expressoes_e_operadores/21_menor_ou_igual/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+if(4 < 12){
+  echo " 4 é menor que 12 <br>";
+}
+
+if(4 < 3){
+  echo " 4 é menor que 3 <br>";
+}
+
+if(12 <= 12){
+  echo " 12 é menor ou igual que 12 <br>";
+}
+
+$a = 5;
+$b = 18;
+$c = 3;
+
+if($a <= $b){
+  echo " $a é menor ou igual que $b <br>";
+}
+
+if($a <= $c){
+  echo " $a é menor ou igual que $c <br>";
+}
