6ad35975cdbeba3d91f09810742006c7e4f30ba6
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-04 08:09:49 +0200

    php studies

---
+++ 5_expressoes_e_operadores/23_exercicio_17/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$a = 15;
+$b = 5;
+$c = "João";
+$d = "João";
+
+
+if($a > $b && $d === $c){
+  echo "a afirmação 1 é verdadeira <br>";
+}
+
+
+if(5 > "teste" &&  10){
+  echo "a afirmação 2 é verdadeira <br>";
+}
+
+if(10 > 5 &&  1){
+  echo "a afirmação 4 é verdadeira <br>";
+}
+
+
+if(2 == 3 && 5 >= 3){
+  echo "a afirmação 3 é verdadeira <br>";
+}
