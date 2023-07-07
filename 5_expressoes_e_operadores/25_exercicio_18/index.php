663798bc2a4d4d10a97051f2ca415e09d192b9e1
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-07 08:31:48 +0200

    php studies

---
+++ 5_expressoes_e_operadores/25_exercicio_18/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if(12 < 5 || "João" === "João"){
+  echo "a afirmação 1 é verdadeira <br>";
+}
+
+if(1 > 5 || 1){
+  echo "a afirmação 2 é verdadeira <br>";
+}
+
+if(20 === "20" && 51 >= 31){
+  echo "a afirmação 3 é verdadeira <br>";
+}
