7878ee35eb862292bea1adeec1fc435e47d0f08a
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-18 09:21:03 +0200

    php studies

---
+++ 6_estruturas_de_controle/1_estrutura_if/index.php
@@ -0,0 +1,21 @@
+<?php
+
+if(5 > 2){
+  echo " verdadeiro <br>";
+}
+
+if(1 > 2){
+  echo " verdadeiro <br>";
+}
+
+if(10 === 10 && 9 > 3){
+  echo " 2 é verdadeiro <br>";
+}
+
+$a = 10;
+$b = 5; 
+$c = "4 deu certo, entrou no if";
+
+if($a > $b){
+  echo $c . "<br>";
+}
