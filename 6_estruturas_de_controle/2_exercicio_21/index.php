636bb812f6ffaa3c41b29e5765e27fdc98240cab
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-19 09:27:10 +0200

    php studies

---
+++ 6_estruturas_de_controle/2_exercicio_21/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$a = 5;
+$b = 2;
+$c = "Matheus";
+$d = "Pedro";
+$e = 12;
+$f = 11;
+
+if($a > $b){
+  echo "afirmação 1 é verdadeira <br>";
+}
+
+if($c !== $d){
+  echo "afirmação 2 é verdadeira <br>";
+}
+
+if($e <= $f){
+  echo "afirmação 3 é verdadeira <br>";
+}
+
