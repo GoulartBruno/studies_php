cc837193adf648e88ebff9803c144c5b3f147dea
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-25 09:46:16 +0200

    php studies

---
+++ 6_estruturas_de_controle/5_exercicio_23/index.php
@@ -0,0 +1,40 @@
+<?php
+
+$a = 18;
+$b = 16;
+$c = 26;
+$d = 10;
+
+$maioridade = 18;
+
+$msg01 = "A pessoa é maior de idade <br>";
+$msg02 = "A pessoa é menor de idade <br>";
+
+
+if($a >= $maioridade){
+  echo "1 ";
+  echo $msg01;
+}else{
+  echo "1 ";
+  echo $msg02;}
+
+if($b >= $maioridade){
+  echo "2 ";
+  echo $msg01;
+}else{
+  echo "2 ";
+  echo $msg02;}
+
+if($c >= $maioridade){
+  echo "3 ";
+  echo $msg01;
+}else{
+  echo "3 ";
+  echo $msg02;}
+
+if($d >= $maioridade){
+  echo "4 ";
+  echo $msg01;
+}else{
+  echo "4 ";
+  echo $msg02;}
