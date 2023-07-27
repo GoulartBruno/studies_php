482fb2dc82418b8f8321e2275c75ad028935a0ae
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-27 09:52:02 +0200

    php studies

---
+++ 6_estruturas_de_controle/6_exercicio_24/index.php
@@ -0,0 +1,51 @@
+<?php
+
+$a = "Bruno";
+$b = "32";
+$c = 10;
+$d = 2.545;
+$e = 18;
+
+$msg01 = "Essa variavel é um numereo inteiro <br>";
+$msg02 = "Essa variavel não é um numereo inteiro<br>";
+
+
+if(is_int($a)){
+echo $msg01;
+}else{
+  echo $msg02;
+}
+
+
+
+if(is_int($b)){
+  echo $msg01;
+  }else{
+    echo $msg02;
+  }
+  
+  
+
+  
+if(is_int($c)){
+  echo $msg01;
+  }else{
+    echo $msg02;
+  }
+  
+  
+  
+if(is_int($d)){
+  echo $msg01;
+  }else{
+    echo $msg02;
+  }
+  
+  
+if(is_int($e)){
+  echo $msg01;
+  }else{
+    echo $msg02;
+  }
+  
+  
