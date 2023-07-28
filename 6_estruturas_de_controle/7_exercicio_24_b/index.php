985ef7f6c0c8daf77c9f5db0a95952beffc54a64
GoulartBruno <brunogoulartdev@gmail.com>
2023-07-28 09:56:21 +0200

    php studies

---
+++ 6_estruturas_de_controle/7_exercicio_24_b/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$peso01 = 50;
+$peso02 = 85;
+
+
+if($peso01 > 80){
+  echo "Isso está pesado demais <br>";
+}else{
+  echo " peso dentro do limite <br>";
+}
+
+if($peso02 > 80){
+  echo "Isso está pesado demais <br>";
+}else{
+  echo " peso dentro do limite <br>";
+}
