aa62e25928bb319d4564848ffde3d3dddd65cb47
GoulartBruno <brunogoulartdev@gmail.com>
2023-08-02 10:53:17 +0200

    php studies

---
+++ 6_estruturas_de_controle/10_else_if/index.php
@@ -0,0 +1,55 @@
+<?php
+
+
+if ( 10 > 5){
+
+  echo "entrou no primerio  if <br>";
+
+}else if (10 > 5){
+
+echo "Entrou no primerio else if <br>";
+
+}
+
+if ( 10 < 5){
+
+  echo "entrou no segundo if <br>";
+
+}else if (10 > 5){
+
+echo "Entrou no segundo else if <br> ";
+
+}
+
+
+if ( 10 < 5){
+
+  echo "entrou no primeiro if <br>";
+
+}else if (10 < 5){
+
+echo "Entrou no segundo else if <br>";
+
+}else{
+
+  echo "Entrou no else <br> ";
+  
+  }
+
+  if ( 10 < 5){
+
+    echo "entrou no primeiro if <br>";
+  
+  }else if (10 < 5){
+  
+  echo "Entrou no segundo else if <br>";
+  
+  }else if (4 < 5){
+  
+    echo "Entrou no terceiro else if <br>";
+    
+    }else{
+  
+    echo "Entrou no terceiro else <br> ";
+    
+    }
