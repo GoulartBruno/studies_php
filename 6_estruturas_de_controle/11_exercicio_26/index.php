4c545e8022c47e2c28cda4c63674398dcc72bc84
GoulartBruno <brunogoulartdev@gmail.com>
2023-08-03 11:01:12 +0200

    php studies

---
+++ 6_estruturas_de_controle/11_exercicio_26/index.php
@@ -0,0 +1,53 @@
+<?php
+
+
+$velocidade01 = 100;
+$velocidade02 = 40;
+$velocidade03 = 30;
+
+$velMax = 40;
+
+
+if($velocidade01 < $velMax){
+
+echo "O carro está dentro da velocidade correta <br>";
+
+}else if ($velocidade01 === $velMax){
+
+  echo "Velocidade igual a 40, tome cuidado! <br>";
+  
+}else{
+
+  Echo "Você levou uma multa por excesso de velocidade <br>";
+
+}
+
+
+if($velocidade02 < $velMax){
+
+  echo "O carro está dentro da velocidade correta <br>";
+  
+  }else if ($velocidade02 === $velMax){
+  
+    echo "Velocidade igual a 40, tome cuidado! <br>";
+    
+  }else{
+  
+    Echo "Você levou uma multa por excesso de velocidade <br>";
+  
+  }
+
+  
+if($velocidade03 < $velMax){
+
+  echo "O carro está dentro da velocidade correta <br>";
+  
+  }else if ($velocidade03 === $velMax){
+  
+    echo "Velocidade igual a 40, tome cuidado! <br>";
+    
+  }else{
+  
+    Echo "Você levou uma multa por excesso de velocidade <br>";
+  
+  }
