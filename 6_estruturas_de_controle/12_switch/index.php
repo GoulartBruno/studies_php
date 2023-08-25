34c33d3608a25245250f92de2afa561945f578a2
GoulartBruno <brunogoulartdev@gmail.com>
2023-08-25 11:58:16 +0200

    php studies

--- 6_estruturas_de_controle/12_switch/index.php
+++ 6_estruturas_de_controle/12_switch/index.php
@@ -19,3 +19,32 @@ switch($x){
   default:
   echo "X não é igual a nenhum dos valores mencionados <br>";
 }
+
+
+?>
+
+<?php
+
+function verificarCategoria($categoria) {
+    
+    switch ($categoria) {
+        case 'eletrônicos':
+          return  "Essa categoria é de produtos eletrônicos";
+            break;
+         case 'vestuário':
+          return  "Essa categoria é de produtos de vestuário";
+            break;
+        case 'alimentícios':
+          return  "Essa categoria é de produtos alimentícios";
+            break;
+        
+        default:
+        return  "Categoria desconhecida";
+         break;
+         }
+}
+
+ echo verificarCategoria('alimentícios');
+
+
+         ?>
