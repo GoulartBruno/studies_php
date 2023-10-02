e5ccbfded4d111e2904d5df144e255802569170e
GoulartBruno <brunogoulartdev@gmail.com>
2023-10-02 15:35:27 +0200

    phu studies

---
+++ 8_inclusao_de_arquivos/3_include_once_require_once/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//Arquivos que não existam
+
+include_once "teste2.php";
+include_once "teste2.php";
+
+//arquivo que não existe
+require_once "teste3.php";
+require_once "teste3.php";
+
+
+?>
+
+<P>Testando pagina</P>
---
+++ 8_inclusao_de_arquivos/3_include_once_require_once/teste3.php
@@ -0,0 +1,2 @@
+
+<p>DAliiiii</p>;
