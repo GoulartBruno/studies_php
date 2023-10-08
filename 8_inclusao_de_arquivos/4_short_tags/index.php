67815428413d718359eff2ed59428b204a125a69
GoulartBruno <brunogoulartdev@gmail.com>
2023-10-08 15:53:19 +0200

    php studies

---
+++ 8_inclusao_de_arquivos/4_short_tags/index.php
@@ -0,0 +1,12 @@
+<?php
+echo "testando codigo PHP nas tags normais"
+
+?>
+
+
+<?
+//não usar
+
+echo "testando codigo PHP nas tags normais"
+
+?>
---
+++ 8_inclusao_de_arquivos/5_exibicao_conteudo/index.php
@@ -0,0 +1,20 @@
+<?php
+/*usar assim:
+
+<?= $nome; ?> 
+
+para imprimir */
+
+
+$nome = "Bruno";
+
+?>
+
+<form action="">
+  <div>
+    <input type="text" value= "<?= $nome; ?>">
+  </div>
+  <div>
+    <input type="submit" value="Enviar">
+  </div>
+</form>
