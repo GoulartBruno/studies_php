99af8e58e5de84f32780a2b5ac731b07245d4d23
GoulartBruno <brunogoulartdev@gmail.com>
2023-10-26 08:46:24 +0200

    php studies

---
+++ 9_funcoes/10_exercicio_35/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function saberNumero($num){
+
+  $result= $num ** 2;
+  
+  return  $result;
+
+}
+
+$x = saberNumero(4);
+$y = saberNumero(5);
+
+
+echo $x . "<br>";
+echo $y . "<br>";;
