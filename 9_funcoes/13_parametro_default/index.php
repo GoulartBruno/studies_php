11e658f1ad4733f68f9e4edcdca7611cc9a884d0
GoulartBruno <brunogoulartdev@gmail.com>
2023-11-08 10:35:18 +0100

    php studies

---
+++ 9_funcoes/13_parametro_default/index.php
@@ -0,0 +1,19 @@
+<?php
+
+function teste($a = "teste"){
+
+  echo "o valor de A é $a <br>";
+
+
+}
+
+teste();
+
+function teste2($b, $a = "teste"){
+
+  echo "o valor de A é $a e de B é $b <br>";
+
+
+}
+
+teste2(12);
